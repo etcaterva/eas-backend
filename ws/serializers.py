4b5d209b691f21db411f61091d4103ee9807880a
Mario Corchero <mcorcherojim@bloomberg.net>
2018-07-01 19:52:11 +0100

    Refactor serializers
    Refactor seralizers in preparation for other draw types.

--- ws/serializers.py
+++ ws/serializers.py
@@ -3,21 +3,17 @@ from rest_framework import serializers
 from . import models
 
 
-class RandomNumberSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.RandomNumber
-        fields = ('id', 'created_at', 'updated_at', 'title', 'description',
-                  'range_min', 'range_max', 'results',)
-
-    RESULT_CLASS = models.RandomNumberResult
+class BaseSerializer(serializers.ModelSerializer):
+    BASE_FIELDS = ('id', 'created_at', 'updated_at', 'title', 'description',
+                   'results',)
 
     results = serializers.SerializerMethodField()
 
     @classmethod
     def get_results(cls, instance):
         return [
-            RandomNumberResultSerializer(result).data
-            for result in cls.RESULT_CLASS.objects
+            cls.RESULT_SERIALIZER(result).data
+            for result in cls.RESULT_MODEL.objects
                 .filter(draw_id=instance.id)
                 .order_by("-created_at")
         ]
@@ -27,3 +23,12 @@ class RandomNumberResultSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.RandomNumberResult
         fields = ('created_at', 'value',)
+
+
+class RandomNumberSerializer(BaseSerializer):
+    class Meta:
+        model = models.RandomNumber
+        fields = BaseSerializer.BASE_FIELDS + ('range_min', 'range_max',)
+
+    RESULT_MODEL = models.RandomNumberResult
+    RESULT_SERIALIZER = RandomNumberResultSerializer
