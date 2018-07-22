de6a234976a2b04f4297ee17d4c9f775418232a1
Mario Corchero <mcorcherojim@bloomberg.net>
2018-07-22 14:30:52 +0100

    Inherit all dev settings from prod
    To ensure we catch all issues before prod, the settings of dev just
    inherit from prod and can configure specific parameters.

--- eas/settings/dev.py
+++ eas/settings/dev.py
@@ -1,11 +1,2 @@
 """Settings for deployment to the DEV/TEST server"""
-from .base import *
-
-ALLOWED_HOSTS = []
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(ROOT_DIR / 'db.sqlite3'),
-    }
-}
+from .prod import *
