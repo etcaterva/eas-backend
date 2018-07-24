86f3bb97184789ea5c96855faca89bbf0fe09cb4
Mario Corchero <mcorcherojim@bloomberg.net>
2018-07-24 14:56:40 +0100

    Add logging traces and configuration
    Add basic logging configuration to the server to help troubleshooting
    issues.

--- eas/api/views.py
+++ eas/api/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins, viewsets, status
@@ -7,6 +9,9 @@ from rest_framework.exceptions import ValidationError
 from drf_yasg.utils import swagger_auto_schema
 
 from . import models, serializers
+
+
+LOG = logging.getLogger(__name__)
 
 
 class BaseDrawViewSet(mixins.CreateModelMixin,
@@ -21,10 +26,12 @@ class BaseDrawViewSet(mixins.CreateModelMixin,
             data.pop(attr)
 
     def create(self, request, *args, **kwargs):
+        LOG.info("Creating new draw for request: %s", request.data)
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        draw = serializer.save()
         headers = self.get_success_headers(serializer.data)
+        LOG.info("Created draw %s", draw)
         return Response(serializer.data, status=status.HTTP_201_CREATED,
                         headers=headers)
 
@@ -35,18 +42,21 @@ class BaseDrawViewSet(mixins.CreateModelMixin,
             return get_object_or_404(self.MODEL, private_id=pk)
 
     def retrieve(self, request, *args, pk=None, **kwargs):  # pylint: disable=arguments-differ
+        LOG.info("Retrieving draw by id: %s", pk)
         instance = self._get_draw(pk)
         instance.resolve_scheduled_results()
         serializer = self.get_serializer(instance)
         result_data = serializer.data
         if pk != result_data["private_id"]:
             self.remove_private_fields(result_data)
+        LOG.info("Returning draw %s", instance)
         return Response(result_data)
 
     @swagger_auto_schema(methods=['post'],
                          request_body=serializers.DrawTossPayloadSerializer)
     @action(methods=['post'], detail=True)
     def toss(self, request, pk):
+        LOG.info("Tossing draw with id: %s", pk)
         draw = get_object_or_404(self.MODEL, private_id=pk)
         serializer = serializers.DrawTossPayloadSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -56,6 +66,7 @@ class BaseDrawViewSet(mixins.CreateModelMixin,
         else:
             result = self._toss_draw(draw)
         result_serializer = serializers.ResultSerializer(result)
+        LOG.info("Generated result: %s", result)
         return Response(result_serializer.data)
 
     def _toss_draw(self, draw):  # pylint: disable=no-self-use
@@ -77,10 +88,12 @@ class RaffleViewSet(BaseDrawViewSet):
 
     @action(methods=['post'], detail=True)
     def participants(self, request, pk):
+        LOG.info("Adding participant to draw %s", pk)
         draw = self._get_draw(pk)
         serializer = serializers.ParticipantSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save(draw=draw)
+        LOG.info("Participant %s added", request.data)
         return Response({}, status.HTTP_201_CREATED)
 
     def _toss_draw(self, draw):
--- eas/settings/base.py
+++ eas/settings/base.py
@@ -131,6 +131,43 @@ SWAGGER_SETTINGS = {
     'DEFAULT_INFO': 'eas.api.urls.api_info',
 }
 
+# Logging Configuration
+LOGGING = {
+    'version': 1,
+    'disable_exiting_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(asctime)s.%(msecs)03d] %(levelname)s %(name)s %(filename)s:%(lineno)s | %(message)s',
+            'datefmt': '%Y-%m-%dT%H:%M:%S',
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'eas': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+
+}
+
 # Secret key generation
 SECRET_FILE = str(APP_DIR / '.secret.txt')
 try:
