f3840fb76d5e96e4d7215433d94a4515ad9ffb58
Mario Corchero <mariocj89@gmail.com>
2018-08-12 16:32:54 +0100

    Configure Sentry to report errors
    Configure sentry to report errors to the framework that will trigger
    emails with information about the incident. An environment variable is
    needed to configure sentry, it has to be set on startup.

--- eas/settings/prod.py
+++ eas/settings/prod.py
@@ -1,4 +1,8 @@
 """Production deployment settings"""
+import os
+
+import raven
+
 from .base import *
 
 DATABASES = {
@@ -9,4 +13,17 @@ DATABASES = {
         'HOST': 'db',
         'PORT': 5432,
     }
+}
+
+
+# Sentry config
+INSTALLED_APPS = [
+    *INSTALLED_APPS,
+    'raven.contrib.django.raven_compat',
+]
+
+RAVEN_CONFIG = {
+    'dsn': os.environ.get('SENTRY_DSN'),
+    'release': raven.fetch_git_sha(ROOT_DIR),
+    'environment': 'prod',
 }
