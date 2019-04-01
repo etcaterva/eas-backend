1bf3b75de6f3fafeae45302051276368e12d3fef
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2019-04-01 21:01:15 +0200

    Add logging configuration to settings.prod
    Mostly copied what we had on eas2, and improved it a bit.

--- eas/settings/prod.py
+++ eas/settings/prod.py
@@ -27,3 +27,56 @@ RAVEN_CONFIG = {
     'release': raven.fetch_git_sha(ROOT_DIR),
     'environment': 'prod',
 }
+
+# Logging Configuration
+BASE_LOG_PATH = os.environ.get("ECHALOASUERTE_LOGS_PATH", "/var/log/echaloasuerte/")
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '[%(asctime)s.%(msecs)03d] %(levelname)s %(name)s:%(module)s.%(funcName)s | %(message)s',
+            'datefmt': '%Y%m%d %H:%M:%S',
+        },
+    },
+    'handlers': {
+        'log_file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(BASE_LOG_PATH, 'echaloasuerte_log.txt'),
+            'maxBytes': 1024 * 1024 * 30,  # 30 MB
+            'backupCount': 5,
+        },
+        'error_log_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(BASE_LOG_PATH, 'echaloasuerte_err.txt'),
+            'maxBytes': 1024 * 1024 * 30,  # 30 MB
+            'backupCount': 5,
+        },
+        'debug_log_file': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(BASE_LOG_PATH, 'echaloasuerte_debug.txt'),
+            'maxBytes': 1024 * 1024 * 30,  # 30 MB
+            'backupCount': 5,
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['debug_log_file'],
+            'level': 'DEBUG',
+        },
+        'eas': {
+            'handlers': ['debug_log_file'],
+            'level': 'DEBUG',
+        },
+        '': {
+            'handlers': ['log_file', 'error_log_file'],
+            'level': 'DEBUG',
+        },
+    }
+}
