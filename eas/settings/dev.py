38194dc4af7bfe81a33b5ef165aac5c4e6b761da
Pedro Alvarez <pedro.alvarez@softiron.com>
2023-12-03 19:57:53 +0000

    Set test SQS queue in dev environment

--- eas/settings/dev.py
+++ eas/settings/dev.py
@@ -9,3 +9,7 @@ PAYPAL_MODE = "sandbox"
 PAYPAL_ID = (
     "AUoNbxkShLicONf0kssMlkgUo91p2x-62izyrGc0YGpUDvrR2CtW0RjWAN0dX6qR2RTAkeWMIq2R0dYa"
 )
+SECRET_SANTA_QUEUE_URL = os.environ.get(
+    "EAS_SQS_SS_QUEUE_URL",
+    "https://sqs.us-east-2.amazonaws.com/059860094276/eas-backend-secret-santa-email-test",
+)
