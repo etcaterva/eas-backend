523a561d88612a4c0d1486cb98a74309278608ad
Mario Corchero <mcorcherojim@bloomberg.net>
2024-05-19 22:05:30 +0200

    Add better support for tournament

--- eas/api/tests/int/test_tournament.py
+++ eas/api/tests/int/test_tournament.py
@@ -1,3 +1,5 @@
+from unittest.mock import ANY
+
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APILiveServerTestCase
@@ -56,3 +58,88 @@ class TestTournament(DrawAPITestMixin, APILiveServerTestCase):
         assert len(draw.participants) == 2
         assert draw.participants[1].name == "ramon"
         assert draw.participants[1].facebook_id == "this_is_an_id"
+
+    def test_success_2part_valid_result(self):
+        draw = self.Factory(participants=[dict(name=name) for name in ["1", "2"]])
+        url = reverse(f"{self.base_url}-toss", kwargs=dict(pk=draw.private_id))
+        result = self.client.post(url, {}).json()["value"]
+        assert len(result) == 1
+        assert result == [
+            {
+                "id": ANY,
+                "next_match_id": None,
+                "participant_1": {
+                    "facebook_id": None,
+                    "name": ANY,
+                    "id": ANY,
+                },
+                "participant_2": {
+                    "facebook_id": None,
+                    "name": ANY,
+                    "id": ANY,
+                },
+                "score": None,
+                "winner_id": None,
+            }
+        ]
+        assert result[0]["participant_1"]["name"] in ["1", "2"]
+        assert result[0]["participant_2"]["name"] in ["1", "2"]
+        assert result[0]["participant_1"]["name"] != result[0]["participant_2"]["name"]
+
+    def test_success_4_people_has_empty_match(self):
+        draw = self.Factory(
+            participants=[dict(name=name) for name in ["1", "2", "3", "4"]]
+        )
+        url = reverse(f"{self.base_url}-toss", kwargs=dict(pk=draw.private_id))
+        result = self.client.post(url, {}).json()["value"]
+        assert len(result) == 3
+        assert result[0]["next_match_id"] == result[2]["id"]
+        assert result[2] == {
+            "id": 2,
+            "next_match_id": None,
+            "participant_1": None,
+            "participant_2": None,
+            "score": None,
+            "winner_id": None,
+        }
+
+    def test_success_5_people_add_phamtom_matches(self):
+        draw = self.Factory(
+            participants=[dict(name=name) for name in ["1", "2", "3", "4", "5"]]
+        )
+        url = reverse(f"{self.base_url}-toss", kwargs=dict(pk=draw.private_id))
+        result = self.client.post(url, {}).json()["value"]
+        assert len(result) == 7
+        empty_match = {
+            "id": ANY,
+            "next_match_id": ANY,
+            "participant_1": None,
+            "participant_2": None,
+            "score": None,
+            "winner_id": None,
+        }
+        assert result[6] == empty_match
+
+        assert result[0]["participant_1"]
+        assert result[0]["participant_2"]
+        assert result[0]["next_match_id"] == result[4]["id"]
+
+        assert result[1]["participant_1"]
+        assert result[1]["participant_2"] is None
+        assert result[1]["next_match_id"] == result[4]["id"]
+
+        assert result[2]["participant_1"]
+        assert result[2]["participant_2"] is None
+        assert result[2]["next_match_id"] == result[5]["id"]
+
+        assert result[3]["participant_1"]
+        assert result[3]["participant_2"] is None
+        assert result[3]["next_match_id"] == result[5]["id"]
+
+        assert result[4]["participant_1"]["id"] == result[1]["winner_id"]
+        assert result[4]["participant_2"] is None
+        assert result[4]["next_match_id"] == result[6]["id"]
+
+        assert result[5]["participant_1"]["id"] == result[2]["winner_id"]
+        assert result[5]["participant_2"]["id"] == result[3]["winner_id"]
+        assert result[5]["next_match_id"] == result[6]["id"]
