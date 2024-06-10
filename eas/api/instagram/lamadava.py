1c682dcdfc12a9731512a79d4daf39be967cd9d5
Mario Corchero <mcorcherojim@bloomberg.net>
2024-06-10 10:16:50 +0200

    lamadava: Fallback to GQL if v2 fails

--- eas/api/instagram/lamadava.py
+++ eas/api/instagram/lamadava.py
@@ -25,6 +25,16 @@ def _session():  # pragma: no cover
     )
 )
 def fetch_comments(media_pk):  # pragma: no cover
+    try:
+        return _fetch_comments_v2(media_pk)
+    except Exception:  # pylint: disable=W0703
+        LOG.info(
+            "Failed to fetch comments via v2 API, falling back to gql", exc_info=True
+        )
+    return _fetch_comments_gql(media_pk)
+
+
+def _fetch_comments_v2(media_pk):  # pragma: no cover
     LOG.info("Sending request to lamadava for %s", media_pk)
     response = _session().get(
         "https://api.lamadava.com/v2/media/comments",
@@ -45,3 +55,23 @@ def fetch_comments(media_pk):  # pragma: no cover
     except KeyError:
         LOG.warning("Failed lamadava request! %s", response.text)
         raise
+
+
+def _fetch_comments_gql(media_pk):  # pragma: no cover
+    LOG.info("Sending request to lamadava for %s", media_pk)
+    response = _session().get(
+        "https://api.lamadava.com/gql/comments",
+        params={
+            "media_id": media_pk,
+            "amount": 50,
+            "access_key": LAMADAVA_APIK,
+        },
+        timeout=ONE_MINUTE * 2,
+    )
+    if not response.ok:
+        LOG.warning("Failed lamadava request! %s", response.text)
+        with contextlib.suppress(Exception):
+            if response.json()["exc_type"] == "NotFoundError":
+                return []
+    response.raise_for_status()
+    return response.json()
